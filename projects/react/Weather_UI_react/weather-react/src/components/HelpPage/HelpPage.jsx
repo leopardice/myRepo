3379d7832b7a9b543b60b83bc766f334ffd3af01
Figaro <figaro138@gmail.com>
2022-06-18 16:48:39 +0300

    route help

--- projects/react/Weather_UI_react/weather-react/src/components/HelpPage/HelpPage.jsx
+++ projects/react/Weather_UI_react/weather-react/src/components/HelpPage/HelpPage.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const HelpPage = () => {
     return (
-        <div className="help-page">
+        <div className="help-page"
+             style={{
+                 fontSize: "2rem",
+                 paddingBottom: "1rem",
+             }}
+        >
+            <nav>
+                <Link to='/'>Main</Link>
+            </nav>
             If you want to get information on current weather and forecast in a given city, type the name into the search form.
         </div>
     );
