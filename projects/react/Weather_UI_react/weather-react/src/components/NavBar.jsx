d43fd5e328a6ed44e11e66b454a533921a88405a
Igor Panov <igor.panov.hse@gmail.com>
2022-07-27 10:20:24 +0300

    main link

--- projects/react/Weather_UI_react/weather-react/src/components/NavBar.jsx
+++ projects/react/Weather_UI_react/weather-react/src/components/NavBar.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './NavBar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 function NavBar() {
   return (
     <nav>
-      <Link className="link" to="/main">Main</Link>
-      <Link className="link" to="/help">Help</Link>
+      <Link className="link" to="/">
+        Main
+      </Link>
+      <Link className="link" to="/help">
+        Help
+      </Link>
     </nav>
   );
 }
